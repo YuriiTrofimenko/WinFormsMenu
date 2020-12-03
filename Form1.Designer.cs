aae3da1457249ee3ce8930a925b3ee334f13e6ea
YuriiTrofimenko <tyaa@ukr.net>
2020-12-03 17:52:23 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace WinFormsMenu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupsDataGridView = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.saveToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.removeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.groupsDataGridView)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // createToolStripMenuItem
+            // 
+            this.createToolStripMenuItem.AutoToolTip = true;
+            this.createToolStripMenuItem.Name = "createToolStripMenuItem";
+            this.createToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.createToolStripMenuItem.Text = "Create";
+            this.createToolStripMenuItem.ToolTipText = "Создать новый файл XML";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupsDataGridView);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 426);
+            this.splitContainer1.SplitterDistance = 266;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // groupsDataGridView
+            // 
+            this.groupsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.groupsDataGridView.ContextMenuStrip = this.contextMenuStrip1;
+            this.groupsDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupsDataGridView.Location = new System.Drawing.Point(0, 0);
+            this.groupsDataGridView.Name = "groupsDataGridView";
+            this.groupsDataGridView.Size = new System.Drawing.Size(266, 426);
+            this.groupsDataGridView.TabIndex = 0;
+            this.groupsDataGridView.SelectionChanged += new System.EventHandler(this.groupsDataGridView_SelectionChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem1,
+            this.removeToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(154, 48);
+            // 
+            // saveToolStripMenuItem1
+            // 
+            this.saveToolStripMenuItem1.Name = "saveToolStripMenuItem1";
+            this.saveToolStripMenuItem1.Size = new System.Drawing.Size(153, 22);
+            this.saveToolStripMenuItem1.Text = "Save Changes";
+            this.saveToolStripMenuItem.Click += SaveToolStripMenuItem_Click;
+            // 
+            // removeToolStripMenuItem
+            // 
+            this.removeToolStripMenuItem.Name = "removeToolStripMenuItem";
+            this.removeToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.removeToolStripMenuItem.Text = "Remove Group";
+            this.removeToolStripMenuItem.Click += RemoveToolStripMenuItem_Click;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.groupsDataGridView)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void SaveToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView groupsDataGridView;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem removeToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WinFormsMenu
+{
+    public partial class Form1 : Form
+    {
+        private BindingList<GroupModel> groupModels = new BindingList<GroupModel>();
+        private BindingSource groupModelsSource;
+        private XmlDocument xmlDoc = new XmlDocument();
+        private XmlElement document;
+        private int selectedGroupId = -1;
+        public Form1()
+        {
+            InitializeComponent();
+            groupModelsSource = new BindingSource(groupModels, null);
+        }
+
+        private void FillGroupsGrid() {
+            groupsDataGridView.Rows.Clear();
+            xmlDoc.Load("groups.xml");
+            try
+            {
+                /* groupModels.Add(new GroupModel() { Id = 0, Name = "Zero" });
+                groupsDataGridView.DataSource = null;
+                groupsDataGridView.DataSource = groupModels; */
+                document = xmlDoc.DocumentElement;
+
+                foreach (XmlElement groupElement in document.ChildNodes)
+                {
+                    groupModels.Add(
+                        new GroupModel()
+                        {
+                            Id = Int32.Parse(groupElement.GetAttribute("id")),
+                            Name = groupElement.GetElementsByTagName("name")[0].InnerXml
+                        });
+                }
+                groupsDataGridView.DataSource = null;
+                groupsDataGridView.DataSource = groupModelsSource;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FillGroupsGrid();
+        }
+
+        private void groupsDataGridView_SelectionChanged(object sender, EventArgs e)
+        {
+            /* foreach (var cell in groupsDataGridView.SelectedRows?[0].Cells)
+            {
+                Console.WriteLine(cell);
+            } */
+            // Console.WriteLine(groupsDataGridView.Rows);
+            foreach (DataGridViewRow row in groupsDataGridView.Rows)
+            {
+                // Console.WriteLine(row.Selected);
+                if (row.Selected)
+                {
+                    // Console.WriteLine(row.Cells[0].Value);
+                    selectedGroupId = (int)row.Cells[0].Value;
+                    /* foreach (DataGridViewCell cell in row.Cells)
+                    {
+                        Console.WriteLine(cell.Value);
+                    } */
+                    break;
+                }
+            }
+            /* for (int i = 0; i < groupsDataGridView.SelectedRows?[0]?.Cells?.Count; i++)
+            {
+                Console.WriteLine(i);
+                // Console.WriteLine(groupsDataGridView.SelectedRows?[0].Cells[i]);
+            } */
+        }
+
+        private void SaveToolStripMenuItem1_Click(object sender, System.EventArgs e)
+        {
+            
+        }
+
+        private void RemoveToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            if (selectedGroupId != -1)
+            {
+                XmlElement removedXmlElement =
+                document.ChildNodes.Cast<XmlElement>()
+                     .Where(groupElement => Int32.Parse(groupElement.GetAttribute("id")) == selectedGroupId)
+                     .SingleOrDefault();
+                document.RemoveChild(removedXmlElement);
+                xmlDoc.Save("groups.xml");
+                FillGroupsGrid();
+            }
+            else {
+                MessageBox.Show("Select a group before");
+            }
+        }
+    }
+}
